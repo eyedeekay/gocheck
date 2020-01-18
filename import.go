42b0cc4535a0e082cd71c57bef8bde75488277d9
idk <idk@idk.local>
2020-01-17 22:30:24 -0500

    do import better

--- import.go
+++ import.go
@@ -10,6 +10,14 @@ func (c *Check) Import(str string) error {
 	if err != nil {
 		return err
 	}
+	return json.Unmarshal(bytes, c)
+}
+
+func (c *Check) ImportSites(str string) error {
+	bytes, err := ioutil.ReadFile(str)
+	if err != nil {
+		return err
+	}
 	return json.Unmarshal(bytes, c.Sites)
 }
 
